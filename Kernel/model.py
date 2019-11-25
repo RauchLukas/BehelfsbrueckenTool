210d2c72b8c59c14faae4da60960d4e9a4a387c0
Lukas Rauch <lukas.rauch@unibw.de>
2019-11-25 18:12:34 +0100

    add model_code

--- Kernel/model.py
+++ Kernel/model.py
@@ -6,8 +6,10 @@ Authors: Lukas Rauch
 import numpy as np
 import numpy.linalg as la
 from .node import Node
+from .element import Element
 from .material import Material
 from .crosssection import Crosssection
+from .load import Load
 
 
 
@@ -52,7 +54,7 @@ class Model(object):
 
         self.neumann_condition = dict()
 
-        self.loadclass = str
+        self._loadclasses = dict()
 
 
 # === get model information
@@ -132,6 +134,44 @@ class Model(object):
 
         return self._materials[id]
 
+    def get_crosssection(self, id):     	
+        """Get the cross section by its ID.
+
+        Parameters
+        ----------
+        id : int or str
+            ID of the material.
+
+        Returns
+        -------
+        cross section : list
+            Cross section with the given ID.
+        """
+
+        if id not in self._crosssections:
+            raise RuntimeError('The model does not contain a material with id: {}' .format(id))
+
+        return self._crosssections[id]
+
+    def get_loadclass(self, id):     	
+        """Get the loadclass by its ID.
+
+        Parameters
+        ----------
+        id : int or str
+            ID of the loadclass.
+
+        Returns
+        -------
+        loadclass : list
+            loadclass with the given ID.
+        """
+
+        if id not in self._loadclasses:
+            raise RuntimeError('The model does not contain a material with id: {}' .format(id))
+
+        return self._loadclasses[id]
+
 # === modeling
     def add_node(self, id, x, y):
         """Add a two dimensional node to the model.
@@ -143,7 +183,7 @@ class Model(object):
         x : float
             X Coordinate
         y : float
-            Y Coordiante
+            Y Coordinate
         
         Examples
         --------
@@ -159,7 +199,7 @@ class Model(object):
 
     def add_element(self, id, node_a, node_b, crosssection, material):
         """
-        Add a two dimensionla bridge element to the model.
+        Add a two dimensional bridge element to the model.
 
         id : int or str
             Unique ID of the element
@@ -173,10 +213,17 @@ class Model(object):
             Material data of the local cross section
         """
 
-        #TODO ADD ELEMENT
+        if id in self._elements:
+            raise RuntimeError('The model already contains an element with id {}'.format(id))
 
+        if node_a not in self._nodes:
+            raise RuntimeError('The model does not contain a node with id {}'.format(node_a))
 
+        if node_b not in self._nodes:
+            raise RuntimeError('The model does not contain a node with id {}'.format(node_b))
 
+        self._elements[id] = Element(
+            id, self._nodes[node_a], self._nodes[node_b], self._crosssections[crosssection], self._materials[material])
 
     def add_material(self,id, 
             density=0, youngs_modulus=0, fmk=0, ft0k=0, ft90k=0, fc0k=0, fc90k=0):
@@ -187,11 +234,11 @@ class Model(object):
         id : int or str
             Unique matrial ID.
         density : float
-            Material densitiy to calculate the material weight.
+            Material density to calculate the material weight.
         youngs_modulus : float
             Material young's modulus.
         fmk : float
-            Avarage bending stiffness - charactraristic.
+            Avarage bending stiffness - characteristic.
         ft0k : float
             Tension stiffness parallel to material fiber.
         ft90k : float
@@ -200,15 +247,13 @@ class Model(object):
             Compression stiffness parallel to material fiber.
         fc90k : float
             Compression stiffness vertical to material fiber.
-            
         """
 
         if id in self._materials:
             raise RuntimeError('The model already contains a material with id: {}' .format(id))
 
-        self._materials[id] = Material(id, 
-            density=0, youngs_modulus=0, fmk=0, ft0k=0, ft90k=0, fc0k=0, fc90k=0)
-
+        self._materials[id] = Material(
+            id, density, youngs_modulus, fmk, ft0k, ft90k, fc0k, fc90k)
         
     def add_crosssection(self, id, area, Iz):
         """
@@ -220,7 +265,7 @@ class Model(object):
             Unique matrial ID.
         area : float
             Area of the cross section.
-        Iz : flaot
+        Iz : float
             Moment of inertia, rotation around the Z-axis
         """
         
@@ -230,8 +275,41 @@ class Model(object):
         self._crosssections[id] = Crosssection(id, area, Iz)
 
 
+    def add_loadclass(self, id, loadtyp, loadclass):
+        """
+        Add a new loadclass to the model environment.
+        """
 
+        if id in self._loadclasses:
+            raise RuntimeError('The model already contains a loadclass with id: {}' .format(id))
+        
+        self._loadclasses[id] = Load(id, loadtyp, loadclass)
 
+# === solving
+
+    def calc_forces(self, element_id, load_id):
+        """
+        Calculates the bending moment and the shearforces of the passed in element
+        
+        Parameters
+        ----------
+
+        id : str
+            Unique ID of an existing element.
+        """
+
+        if element_id not in self._elements:
+            raise RuntimeError('The model dose not contain an element with element id: {}' .format(element_id))
+        if load_id not in self._loadclasses:
+            raise RuntimeError('The model dose not contain a loadclass with load id: {}' .format(load_id))
+
+        loadtype = self._loadclasses[load_id].loadtyp
+        loadclass = self._loadclasses[load_id].loadclass
+
+        element = self._elements[element_id]
+        element_length = element.get_actual_length()
+
+        return element_length
 
 
 
